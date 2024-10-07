f26e959be0aa460c0bad36b97e5e3224c4d6d879
gumrahsindar <gsindar@gmail.com>
2024-10-07 22:53:59 +0300

    add: calc-form and calc-result components and write HTML markups

---
+++ src/components/CalcForm.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import calcIcon from '../assets/images/icon-calculator.svg'
+</script>
+
+<template>
+  <section id="calc__form">
+    <div class="form__header">
+      <h1>Mortgage Calculator</h1>
+      <button>Clear All</button>
+    </div>
+
+    <form>
+      <div class="form__line">
+        <label for="mortgage-amount">Mortgage Amount</label>
+        <div class="input__wrapper">
+          <div class="input__prefix">$</div>
+          <input type="number" id="mortgage-amount" />
+        </div>
+      </div>
+      <div class="form__line">
+        <div>
+          <label for="mortgage-term">Mortgage Term</label>
+          <input type="number" id="mortgage-term" />
+        </div>
+        <div>
+          <label for="interest-rate">Interest Rate</label>
+          <input type="number" id="interest-rate" />
+        </div>
+      </div>
+      <div class="form__line">
+        <fieldset>
+          <legend>Mortgage Type</legend>
+          <div>
+            <input type="radio" id="fixed" name="mortgage-type" />
+            <label for="fixed">Repayment</label>
+          </div>
+          <div>
+            <input type="radio" id="interest-only" name="mortgage-type" />
+            <label for="interest-only">Interest Only</label>
+          </div>
+        </fieldset>
+      </div>
+      <button>
+        <img :src="calcIcon" alt="Calculate" />
+        Calculate Repayments
+      </button>
+    </form>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#calc__form {
+  padding: 40px;
+}
+</style>
---
+++ src/components/CalcResult.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section id="calc__result">
+    <h2>Your results</h2>
+    <p>
+      Your results are shown below based on the information you provided. To
+      adjust the results, edit the form and click “calculate repayments” again.
+    </p>
+    <div class="results__card">
+      <div>
+        <p>Your monthly repayments</p>
+        <p>£1,797.74</p>
+      </div>
+      <div>
+        <p>Total you'll repay over the term</p>
+        <p>£539,322.94</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/variables' as *;
+#calc__result {
+  padding: 40px;
+  background-color: $slate-900;
+  border-radius: 0 24px 24px 75px;
+}
+</style>
