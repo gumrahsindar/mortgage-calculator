267d84874acffdae1c592634145e8431e3fa3a4c
gumrahsindar <gsindar@gmail.com>
2024-10-10 16:56:30 +0300

    feat: Add FormError component for displaying form errors

---
+++ src/components/FormError.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  error: string | undefined
+}>()
+</script>
+
+<template>
+  <p style="margin-top: 4px" class="text-error text-sm">
+    {{ error }}
+  </p>
+</template>
